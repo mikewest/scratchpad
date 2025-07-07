7051bec1793ec7b446c6f582f5e575d776629a88
Mike West <mkwst@chromium.org>
2025-07-07 08:07:15 +0200

    Updating.

--- x-geolocation/scratchpad-geolocation.js
+++ x-geolocation/scratchpad-geolocation.js
@@ -1,19 +1,29 @@
 class ScratchpadGeolocation extends HTMLElement {
-  static LOCATION_REFRESHED = "location-refreshed";
+  static LOCATION_EVENT = "location";
  
+  static PRECISION_ATTRIBUTE = "precise";
+  static REPETITION_ATTRIBUTE = "watch";
+  static PERSISTENCE_ATTRIBUTE = "persistent";
+
   static Precision = {
     PRECISE: "precise",
     COARSE: "coarse"
   };
 
-  static Persistence = {
-    PERSISTENT: "persistent",
+  static Repetition = {
+    REPEATED: "repeated",
     ONCE: "once"
   };
 
+  static Persistence = {
+    PER_SITE: "per site",
+    PER_VISIT: "per visit",
+  };
+
   #userHasSeenAndAcceptedTheDialog = false;
-  #precision = ScratchpadGeolocation.Precision.COARSE;
-  #persistence = ScratchpadGeolocation.Persistence.ONCE;
+  #precision   = ScratchpadGeolocation.Precision.COARSE;
+  #repetition  = ScratchpadGeolocation.Repetition.ONCE;
+  #persistence = ScratchpadGeolocation.Persistence.PER_VISIT;
 
   #demoData = {
     precise: { latitude: -33.856159, longitude: 151.215256 }, // Sydney Opera House
@@ -25,13 +35,17 @@ class ScratchpadGeolocation extends HTMLElement {
   }
 
   connectedCallback() {
-    this.#precision = this.hasAttribute('precise') ?
+    this.#precision = this.hasAttribute(ScratchpadGeolocation.PRECISION_ATTRIBUTE) ?
       ScratchpadGeolocation.Precision.PRECISE :
       ScratchpadGeolocation.Precision.COARSE;
 
-    this.#persistence = this.hasAttribute('persistent') ?
-      ScratchpadGeolocation.Persistence.PERSISTENT :
-      ScratchpadGeolocation.Persistence.ONCE;
+    this.#repetition = this.hasAttribute(ScratchpadGeolocation.REPETITION_ATTRIBUTE) ?
+      ScratchpadGeolocation.Repetition.REPEATED :
+      ScratchpadGeolocation.Repetition.ONCE;
+
+    this.#persistence = this.hasAttribute(ScratchpadGeolocation.PERSISTENCE_ATTRIBUTE) ?
+      ScratchpadGeolocation.Persistence.PER_SITE :
+      ScratchpadGeolocation.Persistence.PER_VISIT;
 
     this.buildElement();
     this.shadowRoot.querySelector('button').addEventListener('click', _ => { this.handleClick() });
@@ -74,7 +88,7 @@ class ScratchpadGeolocation extends HTMLElement {
         border-radius: 8px;
         border: 0px;
         font-size: 0.8rem;
-        width: 250px;
+        width: 325px;
         padding: 0.5rem 0;
         box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);
       }
@@ -99,8 +113,13 @@ class ScratchpadGeolocation extends HTMLElement {
           border: 0;
           color: #051D49;
           margin: 0.55em auto;
+          padding: 0.5em;
           display: block;
+          font-size: 1rem;
         }
+          dialog button:hover {
+            background-color: #C8D7F0;
+          }
     `;
 
     const button = document.createElement('button');
@@ -109,13 +128,11 @@ class ScratchpadGeolocation extends HTMLElement {
     img.src = "https://fonts.gstatic.com/s/i/short-term/release/googlesymbols/my_location/default/24px.svg";
     button.appendChild(img);
 
-    let buttonString = this.#precision === ScratchpadGeolocation.Precision.PRECISE ?
-      "Share your precise location" :
-      "Share your location";
-
-    if (this.#persistence == ScratchpadGeolocation.Persistence.PERSISTENT) {
-      buttonString += " persistently";
-    }
+    let buttonString = `
+      Share your ${this.#persistence === ScratchpadGeolocation.Persistence.PER_SITE ? "" : "current"}
+      ${this.#precision === ScratchpadGeolocation.Precision.PRECISE ? "precise" : ""}
+      location ${this.#persistence === ScratchpadGeolocation.Persistence.PER_SITE ? "when visiting this site" : ""}
+    `;
 
     const txt = document.createTextNode(buttonString);
     button.appendChild(txt);
@@ -124,52 +141,68 @@ class ScratchpadGeolocation extends HTMLElement {
     root.appendChild(button);
   }
 
+  updateLocation() {
+    this.dispatchEvent(
+      new CustomEvent(ScratchpadGeolocation.LOCATION_EVENT, {
+        detail: this.#demoData[this.#precision]
+      }));
+  }
+
+  disposeOfDialog(dialog, dismissed) {
+    dialog.close();
+    this.shadowRoot.removeChild(dialog);
+    if (dismissed) {
+      this.dispatchEvent(
+        new CustomEvent(ScratchpadGeolocation.LOCATION_EVENT, {
+          detail: { dismissed: true }
+        }));
+    }
+  }
+
+  acceptDialog(dialog) {
+    this.#userHasSeenAndAcceptedTheDialog = true;
+    this.disposeOfDialog(dialog, false);
+    this.updateLocation();
+    if (this.#repetition === ScratchpadGeolocation.Repetition.REPEATED) {
+      setInterval(_ => { this.updateLocation() }, 1000);
+    }
+  }
+
   handleClick() {
     if (this.#userHasSeenAndAcceptedTheDialog) {
-      this.dispatchEvent(
-        new CustomEvent(ScratchpadGeolocation.LOCATION_REFRESHED, {
-          detail: this.#demoData[this.#precision]
-        }));
+      this.updateLocation();
     } else {
       const dialog = document.createElement('dialog');
 
-      const titleString = this.#persistence !== ScratchpadGeolocation.Persistence.PERSISTENT ?
-        `<p>This website can only access your location when you choose to share it.</p>
-         <p>When you share your location with this site, a usage indicator will appear in the address bar.</p>` :
-        `<p>This website wants to know your location while you're visiting the site.</p>
-         <p>When the site accesses your location, a usage indicator will appear in the address bar.</p>`;
+      const titleString = `<p>This website
+          ${
+            this.#persistence === ScratchpadGeolocation.Persistence.PER_VISIT ?
+              "can access your location only when you choose to share it." :
+              "would like to access your location."
+          }
+          </p>
+          <p>When you share your location with this site, a usage indicator will appear in the address bar.</p>`;
 
       // Can we please ship `setHTML(...)`?
       dialog.setHTMLUnsafe(`
         ${titleString}
         <img src="./map-${this.#precision}.svg" alt="You're in Sydney!">
-        <button>Got it</button>
+        ${
+            this.#persistence === ScratchpadGeolocation.Persistence.PER_SITE ?
+                "<button>Allow whenever I'm visiting this site</button>" : ""
+        }
+        <button>Allow only via my explicit action</button>
         <button>Nope</button>
       `);
+
       const buttons = dialog.querySelectorAll('button');
-      buttons[0].addEventListener('click', e => {
-        this.#userHasSeenAndAcceptedTheDialog = true;
-        dialog.close();
-        this.dispatchEvent(
-          new CustomEvent(ScratchpadGeolocation.LOCATION_REFRESHED, {
-            detail: this.#demoData[this.#precision]
-          }));
-        if (this.#persistence === ScratchpadGeolocation.Persistence.PERSISTENT) {
-          setInterval(_ => {
-            this.dispatchEvent(
-              new CustomEvent(ScratchpadGeolocation.LOCATION_REFRESHED, {
-                detail: this.#demoData[this.#precision]
-              }));
-          }, 1000);
+      for (const button of buttons) {
+        if (button === buttons[buttons.length-1]) {
+          button.addEventListener('click', _ => { this.disposeOfDialog(dialog, true); });
+        } else {
+          button.addEventListener('click', _ => { this.acceptDialog(dialog); });
         }
-      });
-      buttons[1].addEventListener('click', _ => {
-        dialog.close();
-        this.dispatchEvent(
-          new CustomEvent(ScratchpadGeolocation.LOCATION_REFRESHED, {
-            detail: { dismissed: true }
-          }));
-      });
+      }
 
       this.shadowRoot.appendChild(dialog);
       dialog.showModal();
